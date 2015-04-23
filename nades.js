cdb5fe536d03de877fdb970882de2155a46b84bb
Lugtech <mingwei.samuel@gmail.com>
2015-04-22 17:45:01 -0700

    Updating and renaming
    added cache and cbble

---
+++ nades.js
@@ -0,0 +1,24 @@
+var maps = {
+	cache: {
+		name: "Cache",
+		img: "http://i.imgur.com/yd7crc6.png",
+		pos_x: -2278, 
+		pos_y: 3469, 
+		rot: 0, 
+		scale: 5.5,
+		nades: [
+			
+		]
+	},
+	cbble: {
+		name: "Cobblestone",
+		img: "http://i.imgur.com/fqX78CB.png",
+		pos_x: -3840, 
+		pos_y: 3072, 
+		rot: 0, 
+		scale: 6.0,
+		nades: [
+			
+		]
+	}
+};
