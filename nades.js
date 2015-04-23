de42592b5599592d3bf7c336281f2f62ce90e874
Mingwei Samuel <Mingwei.Samuel@gmail.com>
2015-04-22 20:22:25 -0700

    first grenade (testing still)

--- nades.js
+++ nades.js
@@ -1,24 +1,37 @@
-var maps = {
+window.maps = {
 	cache: {
 		name: "Cache",
 		img: "http://i.imgur.com/yd7crc6.png",
-		pos_x: -2278, 
-		pos_y: 3469, 
-		rot: 0, 
+		pos_x: -2278,
+		pos_y: 3469,
+		rot: 0,
 		scale: 5.5,
-		nades: [
-			
+
+		smokes: [{
+			loc: [1730, 976, 2000, -100, -330, 16]
+		}],
+		fires: [
+
+		],
+		flashes: [
+
 		]
 	},
 	cbble: {
 		name: "Cobblestone",
 		img: "http://i.imgur.com/fqX78CB.png",
-		pos_x: -3840, 
-		pos_y: 3072, 
-		rot: 0, 
+		pos_x: -3840,
+		pos_y: 3072,
+		rot: 0,
 		scale: 6.0,
-		nades: [
-			
+		smokes: [
+
+		],
+		fires: [
+
+		],
+		flashes: [
+
 		]
 	}
 };
